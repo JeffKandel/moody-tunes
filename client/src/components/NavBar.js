bbf2d68db5c6d0fd77918ae9fba1aef0765d174f
emtseng <etseng42@gmail.com>
2017-05-01 09:17:27 -0400

    fix: new name

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
   render() {
     return (
       <nav className="flexcontainer-horizontal">
-        <h6>TUNE / MOOD</h6>
+        <h6>MOODY TUNES</h6>
         <h6>visualizing the emotional arc of your current Spotify jam</h6>
       </nav>
     )
